f816561bcc952676fc32363fa699f37b83f88f4b
asg0826 <94029405+asg0826@users.noreply.github.com>
2022-01-24 17:19:09 -0500

    Add files via upload

---
+++ Darrow/src/main/java/org/telegram/darrow/Main.java
@@ -0,0 +1,32 @@
+package org.telegram.darrow;
+
+import java.io.IOException;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		
+		/**
+		 * Register a new bot. 
+		 */
+
+        try {
+        
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);    
+            Darrow darrow = new Darrow();
+            botsApi.registerBot(darrow);
+            darrow.startMainTask();
+                      
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }  
+        
+        
+    }
+        
+}
